a740510bae28fa1cc9c5636af2df7074466c47b9
8erberg <jascha.achterberg@icloud.com>
2025-07-26 20:29:34 +0100

    added functions scripts

---
+++ Scripts/Functions/Import/LocationsI.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# File-Name:    LocationsI.py
+# Version:      1.1.1
+
+import warnings
+warnings.warn('Version 1.1.1 of LocationsI.py called - check version number in metadata.', UserWarning)
+
+# Path to be added by user
+baseI = ".../..." #Add path to 'Files' folder here
+baseII = baseI
+
+# Results
+resultsFolderI = baseI+'Results/'
+PreAnalysisI = baseI+'Results/PreAnalysis/'
+CheckupsI = baseI+'Results/Checkups/'
+
+# Data: Full data in project directory
+problemDataII = baseII+"Data/Problemdata/PDvI/"
+spikesDataII = baseII+"Data/Spikes/Spikes_"
+LFPsDataII = baseII+"Data/LFPs/"
+LFPsBlackrockDataII = baseII+"Data/LFPs/Blackrock/"
+LFPSessionImportDataI = baseII+"Data/LFPs/LFPSessionImportI/"
+channelDataII = baseII+"Data/Channeldata/"
+channeldata_sessionsheetsII = baseII+"Data/Channeldata/Sessionsheets/"
+exclusionDataII = baseII+"Data/Exclusiondata/"
+spikesListsII = baseII+'/Data/Spikes/SpikesLists/'
+trialdataII = baseII+"Data/Trialdata/"
+sessiondataIII = baseII+"Data/Sessiondata/"
